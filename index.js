6d8cea21c8db54d65ab5dc45c95a3af6011c3c3e
Tricky Wheat <105251040+trickywheat@users.noreply.github.com>
2022-09-11 19:36:01 +0000

    hard-coded ping works.

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
- const nacl = require('tweetnacl'); 
-  // const fs = require('fs');
-  // const html = fs.readFileSync('index.html', { encoding:'utf8' });
+const nacl = require('tweetnacl'); 
 
-/**
- * Returns an HTML page containing an interactive Web-based
- * tutorial. Visit the function URL to see it and learn how
- * to build with lambda.
- */
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fs = require('node:fs');
+const path = require('node:path');
+let commands = {};
+
 exports.handler = async (event) => {
   console.log(JSON.stringify(event));
 
@@ -18,27 +16,60 @@ exports.handler = async (event) => {
     body: '{"message": "I\'m a teapot"}',
   };
 
-  const signature = event.headers['x-signature-ed25519'];
-  const timestamp = event.headers['x-signature-timestamp'];
-  const isValidRequest = nacl.sign.detached.verify(
-    Buffer.from(timestamp + event.body), 
-    Buffer.from(signature, 'hex'),
-    Buffer.from(process.env.DISCORD_BOT_PUBLIC_KEY, 'hex')
-  );
+  let isValidRequest = false;
+
+  if (event.headers.hasOwnProperty("postman-verify") && (event.headers['postman-verify'] === process.env.POSTMAN_VERIFY)) {
+    isValidRequest = true;
+  } else {
+    // Get valid information
+    const signature = event.headers['x-signature-ed25519'];
+    const timestamp = event.headers['x-signature-timestamp'];
+    const publicKey = process.env.DISCORD_BOT_PUBLIC_KEY;
+    isValidRequest = nacl.sign.detached.verify(
+      Buffer.from(timestamp + event.body), 
+      Buffer.from(signature, 'hex'),
+      Buffer.from(publicKey, 'hex')
+    );
+  }
 
   if (!isValidRequest) {
     responseJson.status = 401;
     responseJson.body = '{"message": "Bad request signature"}';
   } else {
     if ((event.requestContext.http.method === "POST") && (event.body.length > 1)) {
-        const jsonPayload = JSON.parse(event.body);
-        console.log(JSON.stringify(jsonPayload));
-        if (jsonPayload.type == 1) {
-          responseJson.statusCode = 200;
-          responseJson.body = '{"type": 1}';
-        }
+      const requestJSON = JSON.parse(event.body);
+
+      console.log(JSON.stringify(requestJSON));
+      if (requestJSON.type == 1) {
+        // Handle Pings
+        responseJson.statusCode = 200;
+        responseJson.body = '{"type": 1}';
+      } else if (requestJSON.data.name == 'ping') {
+        responseJson.statusCode = 200;
+        responseJson.body = '{"type": 4, "data": { "content": "PONG!" }}';
+      } else {
+        responseJson.statusCode = 200;
+        responseJson.body = '{"type": 4, "data": { "content": "Command does not exist." }}';
+      }
     }
   }
   
   return responseJson;
 };
+
+
+function loadCommands() {
+  const commandsPath = path.join(__dirname);
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js') && file != 'index.js');
+
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    commands[command.discordSlashMetadata.name] = command;
+
+    console.log("Loaded: " + command.discordSlashMetadata.name + ": (TYPE: " + commands[command.discordSlashMetadata.name].type + ") " + commands[command.discordSlashMetadata.name].description);
+    
+  }
+
+}
