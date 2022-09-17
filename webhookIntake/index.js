7109466cae124c0d331e696201971d2d3bb060ac
Tricky Wheat <105251040+trickywheat@users.noreply.github.com>
2022-09-17 14:50:03 +0000

    webhookIntake package working correctly.

---
+++ webhookIntake/index.js
@@ -0,0 +1,55 @@
+const discordInteractions = require('discord-interactions');
+
+exports.handler = async (event) => {
+  console.log("event object: " + JSON.stringify(event));
+
+  let responseJson = {
+    statusCode: 418,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: '{"message": "I am a teapot"}',
+  };
+
+  let isValidRequest = false;
+
+  if (event.headers.hasOwnProperty("authorization") && 
+     (event.headers['authorization'] === "Bearer " + process.env.POSTMAN_VERIFY)) {
+    isValidRequest = true;
+  } else {
+    // Get valid information
+    const signature = event.headers['x-signature-ed25519'];
+    const timestamp = event.headers['x-signature-timestamp'];
+    const publicKey = process.env.DISCORD_BOT_PUBLIC_KEY;
+
+    isValidRequest = discordInteractions.verifyKey(event.body, signature, timestamp, publicKey);
+  }
+
+  if (!isValidRequest) {
+    responseJson.statusCode = 401;
+    responseJson.body = '{"message": "Bad request signature"}';
+  } else {
+    if ((event.requestContext.http.method === "POST") && (event.body.length > 1)) {
+      const requestJSON = JSON.parse(event.body);
+      console.log("Discord webhook Payload: " + JSON.stringify(requestJSON));
+
+      responseJson.statusCode = 200;
+      if (requestJSON.type == 1) {
+        // Handle Pings
+        responseJson.body = '{"type": 1}';
+      } else {
+        const responseBody = {
+          "type": 5,
+          "data": {
+            "flags": 1 << 6
+          }
+        }
+        responseJson.body = JSON.stringify(responseBody);
+      }
+    }
+  }
+
+  console.log("respondJson: " + JSON.stringify(responseJson));
+  return responseJson;
+};
+
