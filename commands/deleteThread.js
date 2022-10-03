cbf80e5089804a885a538c5ac11a1cc54f08a803
Tricky Wheat <105251040+trickywheat@users.noreply.github.com>
2022-10-03 23:14:34 +0000

    some linting fixes.  Lets test before we do more.

--- commands/deleteThread.js
+++ commands/deleteThread.js
@@ -3,48 +3,46 @@
   Type: CHAT_INPUT
 */
 
-const { callbackExecute } = require('./requestHelpMessage');
-
-require('dotenv').config({"path": "../.env" });
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+require('dotenv').config({ 'path': '../.env' });
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 module.exports = {
-  "discordSlashMetadata": {
-    "name": "deleteThread",
-    "type": 1, 
-    "description": "Delete Thread",
+  'discordSlashMetadata': {
+    'name': 'deleteThread',
+    'type': 1,
+    'description': 'Delete Thread',
   },
 
   async execute(requestJSON, lambdaEvent, lambdaContext) {
-    let responseJson = {
-      "type": 5,
-      "data": {
-        "flags": 1 << 6
-      }
+    const responseJson = {
+      'type': 5,
+      'data': {
+        'flags': 1 << 6,
+      },
     };
 
     // Build Lambda Payload
     const payloadJSON = {
-        "callbackExecute": lambdaEvent.callbackExecute + 1 || 1,
-        "headers": lambdaEvent.headers,
-        "body": lambdaEvent.body
+      'callbackExecute': lambdaEvent.callbackExecute + 1 || 1,
+      'headers': lambdaEvent.headers,
+      'body': lambdaEvent.body,
     };
 
     const commandInput = {
       FunctionName: lambdaContext.invokedFunctionArn,
-      InvocationType: "Event",
-      Payload: JSON.stringify(payloadJSON)
-    }
+      InvocationType: 'Event',
+      Payload: JSON.stringify(payloadJSON),
+    };
 
     // Set up LambdaClient
-    const { LambdaClient, InvokeCommand } = require("@aws-sdk/client-lambda");
-    const client = new LambdaClient({ region: "us-east-1" });
+    const { LambdaClient, InvokeCommand } = require('@aws-sdk/client-lambda');
+    const client = new LambdaClient({ region: 'us-east-1' });
 
     console.log('Invoking lambda: ' + commandInput.FunctionName);
     const command = new InvokeCommand(commandInput);
     try {
       const lambdaResponse = await client.send(command);
-      console.log("Response from Lambda: " + JSON.stringify(lambdaResponse));
+      console.log('Response from Lambda: ' + JSON.stringify(lambdaResponse));
 
       if (lambdaResponse.StatusCode != 202) {
         responseJson.type = 4;
@@ -58,17 +56,18 @@ module.exports = {
 
     return responseJson;
   },
-    
-  // Since this is executed by Lambda, a Discord-type response is not needed, 
+
+  // Since this is executed by Lambda, a Discord-type response is not needed,
   // but let's follow the convention
   async callbackExecute(requestJSON, lambdaEvent) {
-    console.log("deleteThread - callbackExecute");
-    let responseJson = {
-      "type": 4,
-      "data": {
-        "content": "Lambda callback executed.  requestId `" + lambdaEvent.awsRequestId + "`",
-        "flags": 1 << 6  // Ephemeral message -- viewable by invoker only
-      }
+    console.log('deleteThread - callbackExecute');
+    // Ephemeral message -- viewable by invoker only
+    const responseJson = {
+      'type': 4,
+      'data': {
+        'content': 'Lambda callback executed.  requestId `' + lambdaEvent.awsRequestId + '`',
+        'flags': 1 << 6,
+      },
     };
 
     const guildMember = requestJSON.member.nick || requestJSON.member.user.username;
@@ -77,31 +76,31 @@ module.exports = {
 
     // Get message contents
     const messageContentsJSON = await getRequestMessageContents(threadChannelId);
-    
+
     // Send edited message
-    const editMessageResponseJSON = await editInitialMessageEmbed(messageContentsJSON, guildMember);
+    await editInitialMessageEmbed(messageContentsJSON, guildMember);
 
     // Delete Thread
-    const deleteThreadJSON = await deleteThread(threadChannelId);
+    await deleteThread(threadChannelId);
     return responseJson;
-  }
+  },
 };
 
 
 async function editInitialMessageEmbed(messageContentsJSON, guildMember) {
-  let messageEmbed = messageContentsJSON.embeds;
+  const messageEmbed = messageContentsJSON.embeds;
 
-  messageEmbed[0].title += " -- FULFILLED!";
+  messageEmbed[0].title += ' -- FULFILLED!';
   messageEmbed[0].color = 0xb73939;
-  messageEmbed[0].footer.text += " -- Thread closed by " + guildMember;
+  messageEmbed[0].footer.text += ' -- Thread closed by ' + guildMember;
 
   const url = `https://discord.com/api/v10/channels/${messageContentsJSON.channel_id}/messages/${messageContentsJSON.id}`;
 
   const payloadJSON = {
-    "embeds": messageEmbed
-  }
-  
-  console.log("Send edited initial embeded message...");
+    'embeds': messageEmbed,
+  };
+
+  console.log('Send edited initial embeded message...');
   const editMessageResponseJSON = await sendPayloadToDiscord(url, payloadJSON, 'patch');
   return editMessageResponseJSON;
 }
@@ -110,12 +109,12 @@ async function editInitialMessageEmbed(messageContentsJSON, guildMember) {
 async function getRequestMessageContents(channelId) {
   // Get thread info
   let url = `https://discord.com/api/v10/channels/${channelId}`;
-  console.log("Getting channel information for threadId: " + channelId);
+  console.log('Getting channel information for threadId: ' + channelId);
   const threadChannelInfoJSON = await sendPayloadToDiscord(url, {}, 'get');
-  console.log("Thread's Parent: " + threadChannelInfoJSON.parent_id);
+  console.log('Thread\'s Parent: ' + threadChannelInfoJSON.parent_id);
 
   url = `https://discord.com/api/v10/channels/${threadChannelInfoJSON.parent_id}/messages/${channelId}`;
-  console.log("Getting message contents for initial embed...")
+  console.log('Getting message contents for initial embed...');
   const messageContentsJSON = await sendPayloadToDiscord(url, {}, 'get');
 
   return messageContentsJSON;
@@ -127,15 +126,15 @@ async function deleteThread(channelId) {
 
   const deleteResponse = await sendPayloadToDiscord(url, {}, 'delete');
 
-  return responseJson;  
+  return deleteResponse;
 }
 
 async function sendPayloadToDiscord(url, body, method = 'post') {
-  let payloadJSON = {
-    "method": method,
-    "headers": {
-      "Accept": '*/*'
-    }
+  const payloadJSON = {
+    'method': method,
+    'headers': {
+      'Accept': '*/*',
+    },
   };
 
   if (Object.keys(body).length > 0) {
@@ -144,24 +143,23 @@ async function sendPayloadToDiscord(url, body, method = 'post') {
   }
 
   // Webhook URLs have the discord bot app ID.
-  if (url.indexOf(process.env.DISCORD_BOT_APP_ID) == -1)
-    payloadJSON.headers['Authorization'] = `Bot ${process.env.DISCORD_BOT_TOKEN}`;
+  if (url.indexOf(process.env.DISCORD_BOT_APP_ID) == -1) {payloadJSON.headers['Authorization'] = `Bot ${process.env.DISCORD_BOT_TOKEN}`;}
 
-  console.log("Sending to url: " + method + " " + url);
-  console.log("Sending payload to Discord: " + JSON.stringify(payloadJSON));
+  console.log('Sending to url: ' + method + ' ' + url);
+  console.log('Sending payload to Discord: ' + JSON.stringify(payloadJSON));
   const payloadResponse = await fetch(url, payloadJSON);
-  console.log("Payload Response: " + JSON.stringify(payloadResponse.headers.get('content-type')));
+  console.log('Payload Response: ' + JSON.stringify(payloadResponse.headers.get('content-type')));
 
   let payloadResponseJSON = {};
   if (payloadResponse.headers.get('content-type') === 'application/json') {
-    payloadResponseJSON = await payloadResponse.json()
+    payloadResponseJSON = await payloadResponse.json();
   } else {
     payloadResponseJSON = {
-      "status": payloadResponse.status, 
-      "statusText": payloadResponse.statusText
+      'status': payloadResponse.status,
+      'statusText': payloadResponse.statusText,
     };
   }
-  console.log("Discord reply: " + JSON.stringify(payloadResponseJSON));
+  console.log('Discord reply: ' + JSON.stringify(payloadResponseJSON));
 
   return payloadResponseJSON;
 }
--- commands/index.js
+++ commands/index.js
@@ -1,5 +1,5 @@
-require('dotenv').config({'path': '../.env' });
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+require('dotenv').config({ 'path': '../.env' });
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const fs = require('node:fs');
 const path = require('node:path');
 
@@ -15,15 +15,15 @@ for (const file of commandFiles) {
   const command = require(filePath);
 
   const requestJSON = {
-    "method": 'post',
-    "headers": {
-      "Authorization": `Bot ${process.env.DISCORD_BOT_TOKEN}`,
-      "Content-Type": "application/json"
+    'method': 'post',
+    'headers': {
+      'Authorization': `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+      'Content-Type': 'application/json',
     },
-    "body": JSON.stringify(command.discordSlashMetadata)
-  }
-  
-  console.log("sending: " + command.discordSlashMetadata.name + ": (TYPE: " + command.discordSlashMetadata.type + ") " + command.discordSlashMetadata.description);
+    'body': JSON.stringify(command.discordSlashMetadata),
+  };
+
+  console.log('sending: ' + command.discordSlashMetadata.name + ': (TYPE: ' + command.discordSlashMetadata.type + ') ' + command.discordSlashMetadata.description);
   const response = fetch(url, requestJSON);
 
   Promise.all([response]).then((values) => { console.log(values); });
