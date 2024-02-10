776f6cc5bbb0d807be698d74097ef40880d06629
Tricky Wheat <105251040+trickywheat@users.noreply.github.com>
2024-02-10 20:12:38 +0000

    Update slash commands workflow

--- .ci/runCommandInstall.bash
+++ .ci/runCommandInstall.bash
@@ -2,12 +2,7 @@
 
 INSTALL_COMMAND=true
 
-# for x in new-commands/**/index.js; do
-#   echo "Attempting to install ${x}"
-#   INSTALL_COMMAND=${INSTALL_COMMAND} node ${x}
-# done
-
-x="new-commands/ping/index.js"
-
-echo "Attempting to install ${x}"
-INSTALL_COMMAND=${INSTALL_COMMAND} node ${x}
+for x in new-commands/**/index.js; do
+  echo "Attempting to install ${x}"
+  INSTALL_COMMAND=${INSTALL_COMMAND} node ${x}
+done
